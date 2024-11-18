5efec01a00b9f50eb67f4ca546f4353482731758
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 15:18:09 -0300

    Exercício 3
    Exercício 3 comentado.

---
+++ Exercício_3.py
@@ -0,0 +1,15 @@
+# 3- Crie um comentário de mais de uma linha:
+
+"""
+Apenas explorando a possibilidade de criar um
+comentário composto de mais de uma linha, que
+permite uma explicação mais elaborada sobre o
+código em questão.
+"""
+
+'''
+Todo comentário composto em Python deve ter seu
+conteúdo entre aspas triplas simples ''' ''' ou
+aspas triplas duplas """ """, de acordo com a
+preferência do desenvolvedor.
+'''
