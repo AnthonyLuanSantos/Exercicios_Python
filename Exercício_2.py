4b029f4ad1bc57c3fa98c6884775791cb21141c3
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 15:09:59 -0300

    Exercício 2
    Exercício 2 comentado.

---
+++ Exercício_2.py
@@ -0,0 +1,9 @@
+# 2- Crie um comentário de no máximo uma linha:
+
+# Exemplo de comentário simples, de até uma linha.
+
+"""
+Comentários curtos em Python devem iniciar com o
+símbolo " # ", dessa forma o conteúdo dessa linha
+de código será simplesmente ignorado pelo interpretador.
+"""
