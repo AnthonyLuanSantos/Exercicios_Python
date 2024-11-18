2b8be677b0d12b64e5c9effc1f251b005200f07b
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 15:29:54 -0300

    Exercício 5
    Exercício 5 comentado.

---
+++ Exercício_5.py
@@ -0,0 +1,16 @@
+# 5- Crie uma variável nome e atribua para a
+# mesma um nome digitado pelo usuário:
+
+nome = input("Por favor, digite o seu nome: ")
+
+"""
+De acordo com o enunciado do exercício, inicialmente
+declaramos uma variável de nome 'nome', que por meio
+da função input() pede que o usuário digite algo, nesse
+caso, seu próprio nome.
+"""
+
+"""
+Ao terminar de digitar e apertar a tecla ENTER, o conteúdo
+digitado pelo usuário estará atribuído à variável nome.
+"""
