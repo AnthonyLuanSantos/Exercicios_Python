f5719d2b54a6c2a8565037bb7d71359548cf04e4
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 16:11:06 -0300

    Exercício 9
    Exercício 9 comentado.

---
+++ Exercício_9.py
@@ -0,0 +1,18 @@
+# 9- Crie uma lista com 5 nomes de pessoas:
+
+nomes = ["Anthony", "Vinícius", "Rafael", "Gabriel", "Adamastor"]
+
+print(nomes)
+
+"""
+Uma lista em Python, tipo de dado muito similar a uma array
+em outras linguagens de programação, é uma estrutura de dados
+que serve para guardar mais de um dado associado a uma 
+variável, sem distinção da quantia nem do do tipo de dado em
+questão, a única ressalva é que para que tal tipo de estrutura
+de dado seja reconhecida corretamente pelo interpretador do 
+Python, a mesma deve obedecer a sintaxe, toda e qualquer lista
+tem seus dados dentro de "[]" colchetes.
+"""
+# O retorno será:
+# ["Anthony", "Vinícius", "Rafael", "Gabriel", "Adamastor"]
