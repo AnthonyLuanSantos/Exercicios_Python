0d8323afe21213b451d0d729a767c308c04bc52f
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 15:52:18 -0300

    Exercício 7
    Exercício 7 comentado.

---
+++ Exercício_7.py
@@ -0,0 +1,21 @@
+# 7- Peça para que o usuário digite um número, em
+# seguida exiba em tela o número digitado:
+
+numero = input("Digite um número: ")
+
+print(f"O número digitado é: {numero}")
+
+"""
+Declarada a variável num, por sua vez parametrizada
+com a função input() que interage com o usuário pedindo
+que o mesmo digite um número, podemos exibir esse número
+em tela via função print().
+"""
+"""
+Usando de f'strings e máscaras de substituição podemos
+criar um retorno um pouco mais elaborado, incorporando
+na string o conteúdo da variável numero.
+"""
+
+# Supondo que o usuário tenha digitado 51, o retorno será:
+# O número digitado é: 51
