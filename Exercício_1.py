4640d070fa79f247ca06f74d8fcb02180a61766b
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 14:00:35 -0300

    Primeiro exercício
    Exercício 1 comentado.

---
+++ Exercício_1.py
@@ -0,0 +1,11 @@
+# 1- Crie três variáveis com três tipos de dados diferentes, respeitando sua sintaxe:
+
+nome = "Anthony Luan" # Variável definida com o tipo string;
+idade = 25 # Variável definida com o tipo int;
+altura = 1.77 # Variável definida com o tipo float;
+
+"""
+Para declarar uma variável basta dar um nome para a mesma,
+seguido do operador de atribuição "=" e do conteúdo a ser
+atribuído para essa variável.
+"""
