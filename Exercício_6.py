5f300f0243491367606caabb2677e9f067652858
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-18 15:44:01 -0300

    Exercício 6
    Exercício 6 comentado.

---
+++ Exercício_6.py
@@ -0,0 +1,24 @@
+# 6- Exiba em tela o valor e o tipo de dado da
+# variável num1: Sendo num1 = 1987.
+
+num1 = 1987
+
+print(num1)
+print(type(num1))
+
+"""
+Inicialmente é declarada uma variável de nome num1,
+que por sua vez tem atribuída o número int 1987.
+Parametrizando a função print() com a variável num1,
+obteremos como retorno o valor da mesma.
+"""
+"""
+Já quando parametrizamos nossa função print() com a
+função type() por sua vez parametrizada com a variável
+num1, o retorno esperado é que seja exibido em tela o 
+tipo de dado assossiado a essa variável.
+"""
+
+# O retorno será:
+# 1987
+# Int
