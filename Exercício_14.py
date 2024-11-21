2262b5e99036a1a36b9688117d485bb4f39a9f0d
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 07:59:22 -0300

    Exercício 14
    Exercício 14 comentado.

---
+++ Exercício_14.py
@@ -0,0 +1,23 @@
+# 14- Realize as operações de multiplicação
+# e de divisão entre os valores das variáveis
+# num1 e num2:
+
+num1 = 61
+num2 = 44
+
+print(num1 * num2)
+print(num1 / num2)
+
+"""
+Usando das mesmas variáveis com seus respectivos
+valores, como nos exemplos anteriores, para
+multiplicarmos os valores dessas variáveis basta
+usar do operador " * ", da mesma forma, para
+realizar a divisão simples dos valores de num1 e num2,
+usamos do operador " / ", obtendo assim o resto
+da divisão.
+"""
+
+# O retorno será:
+# 2684
+# 1.3863636363636365
