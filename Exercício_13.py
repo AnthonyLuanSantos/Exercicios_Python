d66403ed23b0107923e15ab7065c1a0b526573a8
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 07:51:39 -0300

    Exercício 13
    Exercício 13 comentado.

---
+++ Exercício_13.py
@@ -0,0 +1,31 @@
+# 13- Subtraia os valores de num1 e num2:
+
+# Exemplo 1: Resultado negativo.
+num1 = 67
+num2 = 75
+
+print(num1 - num2)
+
+# Exemplo 2: Resultado positivo
+num1 = 121
+num2 = 75
+subtracao = num1 - num2
+
+print(subtracao)
+
+"""
+Para subtrair o valor atribuído a num1 do valor
+atribuído a num2, basta fazer o uso do operador
+" - ", diretamente em nossa função print() ou
+atribuindo esse resultado a uma variável como já
+vimos anteriormente.
+"""
+"""
+Nota: Os resultados podem gerar valores tanto
+negativos quanto positivos para a operação.
+"""
+
+
+# O retorno será:
+# -8
+# 46
