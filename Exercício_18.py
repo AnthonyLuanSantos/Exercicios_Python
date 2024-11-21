5064f6492f3973371ebb2c7b48d0eb1bde261b14
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 08:30:09 -0300

    Exercício 18
    Exercício 18 comentado.

---
+++ Exercício_18.py
@@ -0,0 +1,19 @@
+# 18- Verifique se os valores de num1 e num2
+# são iguais:
+
+num1 = 42
+num2 = 86
+
+print(num1 == num2)
+
+"""
+Para verificar de forma lógica se os valores
+atribuídos a variável num1 é igual ou ao menos
+quivalente ao atribuído a variável num2,
+basta utilizar o operador " == ", que para o
+exemplo apresentado, retornará False (falso),
+uma vez que os valores atribuídos não são iguais.
+"""
+
+# O retorno será:
+# False
