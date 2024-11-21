e7b365b403913c559e97c77e3f184844555fc695
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 08:41:40 -0300

    Exercício 20
    Exercício 20 comentado.

---
+++ Exercício_20.py
@@ -0,0 +1,17 @@
+# 20- Verifique se o valor de num1 é
+# igual ou menor que 100:
+
+num1 = 100
+
+print(num1 <= 100)
+
+"""
+Lembrando que aqui temos uma estrutura condicional
+composta, ou igual ou menor que, bastando apenas
+uma dessas proposições ser verdadeira para que o
+retorno seja True. Em nosso exemplo, num1 não é
+menor, mas é igual a 100, logo o retorno será True.
+"""
+
+# O retorno será:
+# True
