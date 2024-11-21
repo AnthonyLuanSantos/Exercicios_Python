73fbeaca0c1e88609a476542dc51793cd0be35a8
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 08:02:35 -0300

    Exercício 15
    Exercício 15 comentado.

---
+++ Exercício_15.py
@@ -0,0 +1,14 @@
+# 15- Eleve o valor de num1 a oitava
+# potência, sendo num1 = 51:
+
+num1 = 51
+
+print(51 ** 8)
+
+"""
+Para efetuar uma exponênciação em Python
+simplesmente usamos do operador " ** ".
+"""
+
+# O retorno será:
+# 45767944570401
