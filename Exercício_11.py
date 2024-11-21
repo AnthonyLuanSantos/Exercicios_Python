fe6586a59f9c980d1f6fc349bba419bc1aa67fd8
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 07:32:08 -0300

    Exercício 11
    Exercício 11 comentado.

---
+++ Exercício_11.py
@@ -0,0 +1,19 @@
+# 11- Some os valores das variáveis num1 e num2:
+# Sendo num1 = 52 e num2 = 106.
+# Por fim exiba em tela o resultado da soma.
+
+num1 = 52
+num2 = 106
+
+print(num1 + num2)
+
+"""
+Declaradas as variáveis num1 e num2 com seus respectivos
+atributos, podemos diretamente dentro da função print()
+executar a operação de soma destas variáveis, bastando
+instanciar as mesmas em nossa função print() e definindo
+a operação de soma por meio do operador " + ".
+"""
+
+# O retorno será:
+# 158
