9da9ae98770910769f12caa8554d4459a586d696
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 15:10:51 -0300

    Exercício 23
    Exercício 23 comentado.

---
+++ Exercício_23.py
@@ -0,0 +1,19 @@
+# 23- Verifique se o valor de num1 consta nos
+# elementos de lista1:
+# Sendo num1 = 100 e 
+# lista1 = [10, 100, 1000, 10000, 100000].
+
+num1 = 100
+lista1 = [10, 100, 1000, 10000, 100000]
+
+print(num1 in lista1)
+
+"""
+Aqui temos uma expressão lógica fazendo o uso
+do operador " in ", que basicamente nos é útil
+para verificar se um determinado dado/valor
+consta dentro de uma variável/objeto.
+"""
+
+# O retorno será:
+# True
