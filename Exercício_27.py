57de83469cf21a2caae038b8e71dc89d59d8ed7d
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 16:10:32 -0300

    Exercício 27
    Exercício 27 comentado.

---
+++ Exercício_27.py
@@ -0,0 +1,34 @@
+# 27- Crie uma estrutura de repetição que
+# percorre a string "Anthony Luan", exibindo
+# em tela letra por letra desse nome:
+
+for n in "Anthony Luan":
+    print(n)
+    
+"""
+Sempre que estamos percorrendo o conteúdo de uma
+determinada variável que sabemos ter um número
+finito de elementos, podemos fazer a leitura de
+cada um destes elementos, um a um a cada laço de
+repetição, por meio de " for ".
+"""
+"""
+Lembrando que o laço " for " consegue percorrer elementos
+de listas, dicionários e outras estruturas de dados
+parecidas, assim como em caso de uma sitring caractere
+por caractere da mesma como se fosse um elemento individual.
+"""
+
+# O retorno será:
+# A
+# n
+# t
+# h
+# o
+# n
+# y
+# 
+# L
+# u
+# a
+# n
