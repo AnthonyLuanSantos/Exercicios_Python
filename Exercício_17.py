146ed92ca55a17aa7e31fb8b719e3b980413624d
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 08:23:39 -0300

    Exercício 17
    Exercício 17 comentado.

---
+++ Exercício_17.py
@@ -0,0 +1,22 @@
+# 17- Dadas duas variáveis num1 e num2
+# com valores 100 e 89, respectivamente,
+# verifique se o valor de num1 é maior
+# que o de num2:
+
+num1 = 100
+num2 = 89
+
+print(num1 > num2)
+
+"""
+Nesse caso, declaradas as variáveis com
+seus respectivos valores, podemos diretamente
+através da função print() realizar a 
+comparação entre as duas variáveis e verificar
+se num1 realmente possui um valor atribuído
+maior que num2, e visse versa, para isto,
+basta utilizar o operador lógico " > ".
+"""
+
+# O retorno será:
+# True
