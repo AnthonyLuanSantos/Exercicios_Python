ed0ab5fe7f63e83c8d11833f53ef4abdbe313b03
Anthony Luan da Silva Santos <anthonyluan.lized@gmail.com>
2024-11-21 08:36:09 -0300

    Exercício 19
    Exercício 19 comentado.

---
+++ Exercício_19.py
@@ -0,0 +1,19 @@
+# 19- Verifique se os valores de num1 e num2
+# são diferentes:
+
+num1 = 38
+num2 = 24
+
+print(num1 != num2)
+
+"""
+De forma parecida com o exemplo anterior, agora
+usando do operador lógico " != " podemos verificar
+se os conteúdos atribuídos as duas variáveis são
+diferentes. Neste caso, o esperado é que seja
+retornado True (verdadeiro), pois ambos os valores
+das variáveis são diferentes entre si.
+"""
+
+# O retorno será:
+# True
